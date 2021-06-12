8077192353f0c4fa9552127d6b2341d62c901085
Sharp_Mind <redlance988@gmail.com>
2021-06-12 20:53:42 +0300

    Homework 6 completed

--- Lesson_3/client.py
+++ Lesson_3/client.py
@@ -8,11 +8,18 @@ pickle.DEFAULT_PROTOCOL
 addr, port = argv[1], argv[2]
 
 action_tosend = [
-    {"action": "authenticate", "time": str(int(time.time())), "user": {
-        "account_name": "Bobik_Rubika", "password": "TheblastFormThepast"}},
-    {"action": "presence", "time": str(int(time.time())), "type": "status", "user": {
-        "account_name": "Bobik_Rubika", "password": "TheblastFormThepast"}},
-    {"action": "quit"}
+    {
+        "action": "authenticate",
+        "time": str(int(time.time())),
+        "user": {"account_name": "Bobik_Rubika", "password": "TheblastFormThepast"},
+    },
+    {
+        "action": "presence",
+        "time": str(int(time.time())),
+        "type": "status",
+        "user": {"account_name": "Bobik_Rubika", "password": "TheblastFormThepast"},
+    },
+    {"action": "quit"},
 ]
 
 action_on_response = {
@@ -32,41 +39,48 @@ def connect():
     s = socket(AF_INET, SOCK_STREAM)
     s.connect((addr, int(port)))
     # s.connect(('localhost', 8008))
-    s.send(pickle.dumps(
-        action_tosend[0], protocol=None, fix_imports=True, buffer_callback=None))
+    s.send(
+        pickle.dumps(
+            action_tosend[0], protocol=None, fix_imports=True, buffer_callback=None
+        )
+    )
     return s
 
 
 def msg_recieve():
-    data = pickle.loads(s.recv(2000000), fix_imports=True,
-                        encoding="utf-8", errors="strict", buffers=None)
+    data = pickle.loads(
+        s.recv(2000000),
+        fix_imports=True,
+        encoding="utf-8",
+        errors="strict",
+        buffers=None,
+    )
 
-    print('Сообщение от сервера: ', data,
-          ', длиной ', len(data), ' байт')
+    print("Сообщение от сервера: ", data, ", длиной ", len(data), " байт")
 
     return data
 
 
 def answer_send(msg):
     s.send(pickle.dumps(msg, protocol=None, fix_imports=True, buffer_callback=None))
-    print(f'Сообщение {msg} отправлено на сервер.')
+    print(f"Сообщение {msg} отправлено на сервер.")
 
 
-def answer_choise(data, msg=''):
+def answer_choise(data, msg=""):
     for action in list(action_on_response.keys()):
-        if action.find(case) != -1 and action.find(data['response']) != -1:
+        if action.find(case) != -1 and action.find(data["response"]) != -1:
             msg = action_on_response[f'{case}_{data["response"]}']
-            set_case(msg['action'])
+            set_case(msg["action"])
             return msg
 
 
 def keepitrolling():
-    while case != 'quit':
+    while case != "quit":
         answer_send(answer_choise(msg_recieve()))
     s.close()
 
 
 if __name__ == "__main__":
-    case = action_tosend[0]['action']
+    case = action_tosend[0]["action"]
     s = connect()
     keepitrolling()
--- project/test_server.py
+++ project/test_server.py
@@ -4,77 +4,126 @@ import server
 
 
 class AuthCaseFunction(unittest.TestCase):
-
     def testauthenticatecase200(self):
-        r = server.auth_case({"action": "authenticate", "time": str(int(time.time())), "user": {
-            "account_name": "Bobik_Rubika", "password": "TheblastFormThepast"}})
-        self.assertEqual(r, {"response": '200', "alert": "OK"})
+        r = server.auth_case(
+            {
+                "action": "authenticate",
+                "time": str(int(time.time())),
+                "user": {
+                    "account_name": "Bobik_Rubika",
+                    "password": "TheblastFormThepast",
+                },
+            }
+        )
+        self.assertEqual(r, {"response": "200", "alert": "OK"})
 
     def testauthenticatecase402(self):
-        r = server.auth_case({"action": "authenticate", "time": str(int(time.time())), "user": {
-            "account_name": "Kon_v_palto", "password": "igogo123"}})
+        r = server.auth_case(
+            {
+                "action": "authenticate",
+                "time": str(int(time.time())),
+                "user": {"account_name": "Kon_v_palto", "password": "igogo123"},
+            }
+        )
         self.assertEqual(
-            r, {"response": '402', "error": "This could be 'wrong password' or 'no account with that name'"})
+            r,
+            {
+                "response": "402",
+                "error": "This could be 'wrong password' or 'no account with that name'",
+            },
+        )
 
 
 class AuthCase409Function(unittest.TestCase):
     def setUp(self):
-        server.known_users = {'Bobik_Rubika': 'TheblastFormThepast'}
-        server.auth_users = ['Bobik_Rubika']
+        server.known_users = {"Bobik_Rubika": "TheblastFormThepast"}
+        server.auth_users = ["Bobik_Rubika"]
 
     def tearDown(self):
         server.auth_users = []
 
     def testauthincatecase409(self):
-        r = server.auth_case({"action": "authenticate", "time": str(int(time.time())), "user": {
-                             "account_name": "Bobik_Rubika", "password": "TheblastFormThepast"}})
+        r = server.auth_case(
+            {
+                "action": "authenticate",
+                "time": str(int(time.time())),
+                "user": {
+                    "account_name": "Bobik_Rubika",
+                    "password": "TheblastFormThepast",
+                },
+            }
+        )
         self.assertEqual(
-            r, {"response": '409', "error": "Someone is already connected with the given user name"})
+            r,
+            {
+                "response": "409",
+                "error": "Someone is already connected with the given user name",
+            },
+        )
 
 
 class RollmycasesFunction(unittest.TestCase):
-
     def testpresence200(self):
-        data = {"action": "presence", "time": str(int(time.time())), "type": "status", "user": {
-            "account_name": "Bobik_Rubika", "password": "TheblastFormThepast"}}
-        r = server.roll_my_cases(data['action'])
-        self.assertEqual(r, {"response": '200', "alert": "OK"})
+        data = {
+            "action": "presence",
+            "time": str(int(time.time())),
+            "type": "status",
+            "user": {"account_name": "Bobik_Rubika", "password": "TheblastFormThepast"},
+        }
+        r = server.roll_my_cases(data["action"])
+        self.assertEqual(r, {"response": "200", "alert": "OK"})
 
     def testquitcase200(self):
         data = {"action": "quit"}
-        r = server.roll_my_cases(data['action'])
-        self.assertEqual(r, {"response": '200', "alert": "OK"})
+        r = server.roll_my_cases(data["action"])
+        self.assertEqual(r, {"response": "200", "alert": "OK"})
 
 
 class RollmycasesFunctionNotEqual(unittest.TestCase):
-
     def testquitcase(self):
-        self.assertNotEqual(server.roll_my_cases("quit"), '')
+        self.assertNotEqual(server.roll_my_cases("quit"), "")
 
     def testpresensecase(self):
-        self.assertNotEqual(server.roll_my_cases("presence"), '')
+        self.assertNotEqual(server.roll_my_cases("presence"), "")
 
     def testquitcase200(self):
         data = {"action": "qwe"}
-        r = server.roll_my_cases(data['action'])
-        self.assertNotEqual(r, {"response": '200', "alert": "OK"})
+        r = server.roll_my_cases(data["action"])
+        self.assertNotEqual(r, {"response": "200", "alert": "OK"})
 
 
 class AuthCaseFunctionNotEqual(unittest.TestCase):
-
     def testauthcase(self):
-        self.assertNotEqual(server.auth_case({"action": "authenticate", "time": str(int(time.time())), "user": {
-            "account_name": "Bobik_Rubika", "password": "TheblastFormThepast"}}), '')
+        self.assertNotEqual(
+            server.auth_case(
+                {
+                    "action": "authenticate",
+                    "time": str(int(time.time())),
+                    "user": {
+                        "account_name": "Bobik_Rubika",
+                        "password": "TheblastFormThepast",
+                    },
+                }
+            ),
+            "",
+        )
 
 
 class ErrorCaseFunctionEqual(unittest.TestCase):
-
     def testauthcase(self):
-        r = server.roll_my_cases({"action": "destroy", "time": str(int(time.time())), "user": {
-            "account_name": "Bobik_Rubika", "password": "TheblastFormThepast"}})
+        r = server.roll_my_cases(
+            {
+                "action": "destroy",
+                "time": str(int(time.time())),
+                "user": {
+                    "account_name": "Bobik_Rubika",
+                    "password": "TheblastFormThepast",
+                },
+            }
+        )
 
-        self.assertEqual(r, {"response": '404', "error": "Unknown message"})
+        self.assertEqual(r, {"response": "404", "error": "Unknown message"})
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
